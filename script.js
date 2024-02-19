0ae0d5f6cfe4620ae56fe46047d90fe0ba5f01de
fullstackgenerator <simon.hojnik@gmail.com>
2024-02-19 12:16:19 +0100

    drugi update
    Dodan javascript animacija za prikaz ugank.

--- script.js
+++ script.js
@@ -78,3 +78,26 @@ const riddles = [
   
     displayRiddle();
     initializeEvents();
+
+    const the_animation = document.querySelectorAll('.druga')
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('scroll-animation')
+            }
+                else {
+                    entry.target.classList.remove('scroll-animation')
+                }
+            
+        })
+    },
+       { threshold: 0.5
+       });
+    //
+      for (let i = 0; i < the_animation.length; i++) {
+       const elements = the_animation[i];
+    
+        observer.observe(elements);
+      } 
+    
